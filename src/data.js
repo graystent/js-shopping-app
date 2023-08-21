c2fbb4167279a5b2fc7e82fae9783f0cee6031b8
graystent <graystent@gmail.com>
2023-08-21 09:02:32 -0400

    Landing page for shopping site

---
+++ src/data.js
@@ -0,0 +1,41 @@
+export const sliderItems = [
+    {
+      id: 1,
+      img: "https://i5.walmartimages.com/asr/d5b232a7-1d26-48bf-9bea-fa7c4a81551f.bab44da570da6be12a0c199c7fa6be74.jpeg?odnHeight=2000&odnWidth=2000&odnBg=FFFFFF",
+      title: "SUMMER SALE",
+      desc: "DON'T COMPROMISE ON STYLE! GET FLAT 30% OFF FOR NEW ARRIVALS.",
+      bg: "f5fafd",
+    },
+    {
+      id: 2,
+      img: "https://i5.walmartimages.com/asr/e61e9e84-b70a-4d86-aa18-0996453bb592.e94d94d2a93d480a06867300c8654c96.jpeg?odnHeight=2000&odnWidth=2000&odnBg=FFFFFF",
+      title: "AUTUMN COLLECTION",
+      desc: "DON'T COMPROMISE ON STYLE! GET FLAT 30% OFF FOR NEW ARRIVALS.",
+      bg: "fcf1ed",
+    },
+    {
+      id: 3,
+      img: "https://i5.walmartimages.com/asr/db8ac3fe-d36e-497c-ac87-da086ea18453.e8a2ef8e79c54bddb987521002811e90.jpeg?odnHeight=2000&odnWidth=2000&odnBg=FFFFFF",
+      title: "LOUNGEWEAR LOVE",
+      desc: "DON'T COMPROMISE ON STYLE! GET FLAT 30% OFF FOR NEW ARRIVALS.",
+      bg: "fbf0f4",
+    },
+  ];
+
+  export const categories = [
+    {
+      id: 1,
+      img: "https://images.pexels.com/photos/5886041/pexels-photo-5886041.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940",
+      title: "SHIRT STYLE!",
+    },
+    {
+      id: 2,
+      img: "https://images.pexels.com/photos/2983464/pexels-photo-2983464.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940",
+      title: "LOUNGEWEAR LOVE",
+    },
+    {
+      id: 3,
+      img: "https://images.pexels.com/photos/5480696/pexels-photo-5480696.jpeg?auto=compress&cs=tinysrgb&dpr=2&w=500",
+      title: "LIGHT JACKETS",
+    },
+  ];
